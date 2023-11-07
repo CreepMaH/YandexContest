61529cd7915c67787dbd1d3d5b57e79fbf352f41
Сергей Винчестер <creepmah@gmail.com>
2023-11-07 08:41:57 +0300

    Добавьте файлы проекта.

---
+++ YandexContest/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YandexContest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine()?.Trim().Split(' ') ?? throw new ArgumentException();
+            int a = Convert.ToInt32(nums[0]),
+                b = Convert.ToInt32(nums[1]),
+                c = Convert.ToInt32(nums[2]),
+                d = Convert.ToInt32(nums[3]);
+
+            int numerator = a * d + b * c;
+            int denominator = b * d;
+
+            List<int> numeratorSimpleDividers = GetSimpleDividers(numerator);
+            numeratorSimpleDividers.Add(numerator);
+
+            List<int> denominatorSimpleDividers = GetSimpleDividers(denominator);
+            denominatorSimpleDividers.Add(denominator);
+
+            int maxCommonDivider = GetMaxCommonDivider(numeratorSimpleDividers, denominatorSimpleDividers);
+            Console.WriteLine($"{numerator / maxCommonDivider} {denominator / maxCommonDivider}");
+        }
+
+        private static List<int> GetSimpleDividers(int num)
+        {
+            List<int> simpleDividers = new List<int>();
+
+            int currentNum = num;
+            int currentDivider = 2;
+
+            while (currentDivider < num)
+            {
+                int divRest = currentNum % currentDivider;
+                if (divRest == 0)
+                {
+                    currentNum /= currentDivider;
+                    if (!simpleDividers.Contains(currentDivider))
+                    {
+                        simpleDividers.Add(currentDivider);
+                    }
+                }
+                else
+                {
+                    currentDivider++;
+                }
+            }
+            return simpleDividers;
+        }
+
+        private static int GetMaxCommonDivider(List<int> dividers1, List<int> dividers2)
+        {
+            int maxCommonDivider = 1;
+            int minCount = Math.Min(dividers1.Count, dividers2.Count);
+
+            for (int i = 0; i < minCount; i++)
+            {
+                if (dividers1[i] == dividers2[i])
+                {
+                    maxCommonDivider = dividers1[i];
+                }
+            }
+
+            return maxCommonDivider;
+        }
+    }
+}
